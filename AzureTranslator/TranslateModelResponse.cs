24793b71b03ba48d71eff227073303bbff709456
Oybek Kuchkarov <oybekkuch@gmail.com>
2023-04-08 19:59:53 +0500

    Добавьте файлы проекта.

---
+++ AzureTranslator/TranslateModelResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AzureTranslator
+{
+    public class TranslateModelResponse
+    {
+        [JsonPropertyName("translations")]
+        public List<Translation> Translations { get; set; }
+    }
+
+    public class Translation
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+        [JsonPropertyName("to")]
+        public string To { get; set; }
+    }
+
+
+}
