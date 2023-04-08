c8007bc429f125caf6ea9ab47c6fbac114df0c89
Oybek Kuchkarov <oybekkuch@gmail.com>
2023-04-09 01:14:19 +0500

    updated structure

--- AzureTranslator/Program.cs
+++ AzureTranslator/Program.cs
@@ -1,9 +1,11 @@
-﻿using AzureTranslator;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using AzureTranslator.Services;
 
-var builder = new ConfigurationBuilder()
+/*var builder = new ConfigurationBuilder()
     .AddUserSecrets<Program>();
 
 var config = builder.Build();
@@ -35,4 +37,41 @@ async Task<string> PostHttpResponseMessage(string word)
     }
 
     return result;
+}*/
+
+
+
+namespace AzureTranslator
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            var translator = host.Services.GetRequiredService<AzureTranslatorService>();
+            while (true)
+            {
+                Console.Write("Введите текст для перевода:");
+                string inputText = Console.ReadLine();
+
+                string translationText = await translator.TranslateAsync(inputText);
+                Console.WriteLine($"Перевод: {translationText}");
+            }
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureServices((context, collection) =>
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+
+            var config = builder.Build();
+            collection.AddHttpClient("translator", client =>
+            {
+                client.BaseAddress = new Uri("https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=ru&to=en");
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", config["translator:key"]);
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Region", config["translator:location"]);
+            });
+            collection.AddSingleton<AzureTranslatorService>();
+        });
+    }
 }
+
---
+++ AzureTranslator/Services/AzureTranslatorService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using AzureTranslator.Models;
+
+namespace AzureTranslator.Services
+{
+    public class AzureTranslatorService
+    {
+        private readonly IHttpClientFactory _factory;
+        public AzureTranslatorService(IHttpClientFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task<string> TranslateAsync(string text)
+        {
+            var client = _factory.CreateClient("translator");
+            var body = new object[] { new { Text = text } };
+            var requestBody = JsonConvert.SerializeObject(body);
+            var request = new HttpRequestMessage(HttpMethod.Post, client.BaseAddress)
+            {
+                Content = new StringContent(requestBody, Encoding.UTF8, "application/json")
+            };
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+            string content = await response.Content.ReadAsStringAsync();
+            var translationModel = JsonConvert.DeserializeObject<List<TranslateModelResponse>>(content);
+            var translationTexts = translationModel.SelectMany(t => t.Translations.Select(t => t.Text));
+            var result = string.Join(", ", translationTexts);
+            return result;
+        }
+    }
+}
