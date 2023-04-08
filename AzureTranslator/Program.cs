1e39af6f12ea8f7b4ca2d94633e5b656de20d227
Oybek Kuchkarov <oybekkuch@gmail.com>
2023-04-08 21:44:11 +0500

    moved http related stuff to separate function

--- AzureTranslator/Program.cs
+++ AzureTranslator/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System.Text;
 
-
 var builder = new ConfigurationBuilder()
     .AddUserSecrets<Program>();
 
@@ -12,26 +11,28 @@ var config = builder.Build();
 while (true)
 {
     Console.Write("Введите текст для перевода:");
-    string inputText = Console.ReadLine();
-    object[] body = new object[] { new { Text = inputText } };
+    object[] body = new object[] { new { Text = Console.ReadLine() } };
     var requestBody = JsonConvert.SerializeObject(body);
+    var responseModel = JsonConvert.DeserializeObject<List<TranslateModelResponse>>(await PostHttpResponseMessage(requestBody));
+    string resultsJoin = string.Join(", ", responseModel!.SelectMany(x => x.Translations.Select(s => s.Text)));
+    Console.WriteLine($"Перевод: {resultsJoin} ");
+}
 
-    var endpoint = config["translator:endpoint"];
-    var route = config["translator:route"];
 
+async Task<string> PostHttpResponseMessage(string word)
+{
+    var result = string.Empty;
     using (var client = new HttpClient())
     using (var request = new HttpRequestMessage())
     {
         request.Method = HttpMethod.Post;
-        request.RequestUri = new Uri(endpoint + route);
-        request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+        request.RequestUri = new Uri(config["translator:endpoint"]+config["translator:route"]);
+        request.Content = new StringContent(word, Encoding.UTF8, "application/json");
         request.Headers.Add("Ocp-Apim-Subscription-Key", config["translator:key"]);
         request.Headers.Add("Ocp-Apim-Subscription-Region", config["translator:location"]);
         var response = await client.SendAsync(request).ConfigureAwait(false);
-        var result = await response.Content.ReadAsStringAsync();
-        var responseModel = JsonConvert.DeserializeObject<List<TranslateModelResponse>>(result);
-
-        string resultsJoin = string.Join(", ", responseModel!.SelectMany(x => x.Translations.Select(x => x.Text)));
-        Console.WriteLine($"Перевод: {resultsJoin} ");
+        result = await response.Content.ReadAsStringAsync();
     }
+
+    return result;
 }
